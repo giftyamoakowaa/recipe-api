33687cb7f8b77c5bc399a5f3783db380fd845abd
giftyamoakowaa <maameamoakowaa1@gmail.com>
2024-07-10 14:19:41 +0000

    changes

--- index.js
+++ index.js
@@ -3,12 +3,18 @@ import cors from "cors";
 import mongoose from "mongoose";
 import recipeRouter from "./routes/recipes.js";
 import categoryRouter from "./routes/category.js";
+import expressOasGenerator from "express-oas-generator";
 // Connect to database
 await mongoose.connect(process.env.MONGO_URL);
 
 
 // Create Express APP
 const app = express();
+expressOasGenerator.handleResponses(app, {
+    alwaysServeDocs: true, 
+    tags: ['categories', 'recipes'],
+    mongooseModels: mongoose.modelNames(),                                                                                                                                                                                                                                    
+})
 
 // Apply middleware
 app.use(cors ());
@@ -20,6 +26,8 @@ app.use(express.static('uploads'));
 // use routes
 app.use(recipeRouter);
 app.use(categoryRouter);
+expressOasGenerator.handleRequests();
+app.use((req, res) => res.redirect('/api-docs/'));
 
 app.patch('/sign', (req, res) => 
     { res.json('sign up');
